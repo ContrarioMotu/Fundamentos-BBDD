0b3a34cea263f4017d78b3e319198810c36718c1
MaiteDT <53274538+MaiteDT@users.noreply.github.com>
2022-03-19 21:26:50 -0600

    clase cliente

--- Practica02/SRC/Cliente.java
+++ Practica02/SRC/Cliente.java
@@ -1,4 +1,195 @@
 public class Cliente {
-    
-}
 
+  String curp;
+  String apellidoPaterno;
+  String apellidoMaterno;
+  String nombre;
+  String estado;
+  String calle;
+  String cp;
+  String email;
+  String telefono;
+  String fechaNac;
+
+  public Cliente(String curp, String apellidoPaterno, String apellidoMaterno, String nombre,
+    String estado, String calle, String cp, String email, String telefono, String fechaNac) {
+    this.curp = curp;
+    this.apellidoPaterno = apellidoPaterno;
+    this.apellidoMaterno = apellidoMaterno;
+    this.nombre = nombre;
+    this.estado = estado;
+    this.calle = calle;
+    this.cp = cp;
+    this.email = email;
+    this.telefono = telefono;
+    this.fechaNac = fechaNac;
+  }
+
+  /**
+   * Regresa el nombre
+   * @return
+   */
+  public String getNombre() {
+    return nombre;
+  }
+
+  /**
+   * Regresa el apellido paterno
+   * @return
+   */
+  public String getApellidoPaterno() {
+    return apellidoPaterno;
+  }
+
+  /**
+   * Regresa el apellido materno
+   * @return
+   */
+  public String getApellidoMaterno() {
+    return apellidoMaterno;
+  }
+
+  /**
+   * Regresa el CURP
+   * @return
+   */
+  public String getCurp() {
+    return curp;
+  }
+
+  /**
+   * Regresa el estado
+   * @return
+   */
+  public String getEstado() {
+    return estado;
+  }
+
+  /**
+   * Regresa el codigo postal
+   * @return
+   */
+  public String getCP() {
+    return cp;
+  }
+
+  /**
+   * Regresa la calle
+   * @return
+   */
+  public String getCalle() {
+    return calle;
+  }
+
+  /**
+   * Regresa el telefono
+   * @return
+   */
+  public String getTelefono() {
+    return telefono;
+  }
+
+  /**
+   * Regresa la dirección de email
+   * @return
+   */
+  public String getEmail() {
+    return email;
+  }
+
+  /**
+   * Regresa la fecha de nacimiento
+   * @return
+   */
+  public String getFechaNacimiento() {
+    return fechaNac;
+  }
+
+  /**
+   * Regresa un string compuesto que representa la dirección completa
+   * @return
+   */
+  public String getDireccion() {
+    return calle + ". Estado:" + estado + ". CP: " + cp;
+  }
+
+  /**
+   * Asigna el nombre
+   * @param nombre
+   */
+  public setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  /**
+   * Asigna el apellido paterno
+   * @param apellidoPaterno
+   */
+  public setApellidoPaterno(String apellidoPaterno) {
+    this.apellidoPaterno = apellidoPaterno;
+  }
+
+  /**
+   * Asiga el apellido materno
+   * @param apellidoMaterno
+   */
+  public setApellidoMaterno(String apellidoMaterno) {
+    this.apellidoMaterno = apellidoMaterno;
+  }
+
+  /**
+   * Asigna el curp
+   * @param curp
+   */
+  public setCurp(String curp) {
+    this.curp = curp;
+  }
+
+  /**
+   * Asigna el estado
+   * @param estado
+   */
+  public setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  /**
+   * Asigna el codigo postal
+   * @param cp
+   */
+  public setCP(String cp) {
+    this.cp = cp;
+  }
+
+  /**
+   * Asigna la calle
+   * @param calle
+   */
+  public setCalle(String calle) {
+    this.calle = calle;
+  }
+
+  /**
+   * Asigna el numero de telefono
+   * @param telefono
+   */
+  public setTelefono(String telefono) {
+    this.telefono = telefono;
+  }
+
+  /**
+   * Asigna el email
+   * @param email
+   */
+  public setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * Asigna la fecha de nacimiento
+   * @param fechaNac
+   */
+  public setFechaNacimiento(String fechaNac) {
+    this.fechaNac = fechaNac;
+  }
+}

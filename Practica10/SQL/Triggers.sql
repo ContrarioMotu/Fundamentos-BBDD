b5e46a832575aa6567bff6194bfa62f1160e84f4
osrm17 <rg.oscar17@ciencias.unam.mx>
2022-05-25 12:42:07 -0500

    Segundo trigger

--- Practica10/SQL/Triggers.sql
+++ Practica10/SQL/Triggers.sql
@@ -13,9 +13,24 @@ CREATE OR REPLACE FUNCTION invertir_apellidos() RETURNS TRIGGER
 
 
 CREATE TRIGGER invertir_apellidos_trigger
-    BEFORE INSERT on supervisor
+    BEFORE INSERT ON supervisor
     FOR EACH ROW
     EXECUTE PROCEDURE invertir_apellidos();
+
+CREATE OR REPLACE FUNCTION no_update_delete() RETURNS TRIGGER
+    AS $$
+    BEGIN 
+    -- INSERT INTO mascota VALUES (OLD)
+    --RETURN OLD;
+    RAISE EXCEPTION 'NO PERMITIMOS ACTUALIZAR NI BORRAR';
+    END;
+    $$
+    LANGUAGE plpgsql;
     
+
+CREATE TRIGGER no_update_detele_trigger
+    BEFORE UPDATE OR DELETE ON mascota
+    FOR EACH ROW
+    EXECUTE PROCEDURE no_update_delete();
 
 

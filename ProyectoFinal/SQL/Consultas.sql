f7e69f10e1a81af2260ac25699c5487d95d7edc3
MaiteDT <digit@ciencias.unam.mx>
2022-06-15 17:43:14 -0500

    Consultas

---
+++ ProyectoFinal/SQL/Consultas.sql
@@ -0,0 +1,10 @@
+--Consultas
+--Vemos la carta
+SELECT nombre, precioVenta FROM ProductoALimenticio;
+--Nos aseguramos que el burrito vegetariano, de verdad sea vegetariano
+SELECT Preparar.idIngrediente, nombre FROM Preparar INNER JOIN Ingrediente ON Preparar.idIngrediente = Ingrediente.idIngrediente WHERE idProductoALimenticio = 6
+--Recomendaciones de platillos para pico de gallo
+SELECT ProductoAlimenticio.idProductoAlimenticio, ProductoALimenticio.nombre
+FROM RecomendarConSalsa INNER JOIN ProductoAlimenticio
+ON RecomendarConSalsa.idProductoALimenticio = ProductoAlimenticio.idProductoAlimenticio 
+WHERE idSalsa= 5;
